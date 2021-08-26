b3093331a5cf356d30b0aa7858184b737a628671
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2021-08-26 17:55:19 -0300

    api-spring-rest

---
+++ springrestapi/src/main/java/com/br/api/model/Usuario.java
@@ -0,0 +1,31 @@
+package com.br.api.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Usuario implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@EqualsAndHashCode.Include
+	private Long id;
+	
+	private String login;
+	private String senha;
+	private String nome;
+
+}
