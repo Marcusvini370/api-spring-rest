9790e04e06881a8cc9bd96b8e79eaf88abf49d92
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2021-08-26 17:31:54 -0300

    Api-spring-rest

---
+++ springrestapi/src/main/java/com/br/api/SpringrestapiApplication.java
@@ -0,0 +1,27 @@
+package com.br.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EntityScan(basePackages = {"com.br.api.model"}) //varre as entidades do model e as ajuda a ser criada no  bd
+@ComponentScan(basePackages = {"com.*"}) //injeções. ler tudo dentro da pasta com
+@EnableJpaRepositories(basePackages = {"com.br.api.repository"}) //habilita a parte de repositórios do jpa
+@EnableTransactionManagement //Controlar as transações ,resolve bastante problemas
+@EnableWebMvc //habilita os recursos de mvc
+@RestController // indica que o projeto é rest e os controller irá retorna json
+@EnableAutoConfiguration // O srping vai configurar todo o projeto
+public class SpringrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrestapiApplication.class, args);
+	}
+
+}
