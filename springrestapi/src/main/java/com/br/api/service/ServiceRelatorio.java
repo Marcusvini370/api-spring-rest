453f8fbb0f6b3bef76200b2cd33b0558add6c2d3
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2021-09-29 16:59:50 -0300

    Service geração de relatório

---
+++ springrestapi/src/main/java/com/br/api/service/ServiceRelatorio.java
@@ -0,0 +1,50 @@
+package com.br.api.service;
+
+import java.io.File;
+import java.io.Serializable;
+import java.sql.Connection;
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+
+@Service
+public class ServiceRelatorio implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	/*Método que dá uma rebosta de byte array , mostra o pdf direto , e vai receber parametros no relátorio*/
+	public byte[] gerarRelatorio(String nomeRelatorio, Map<String,Object> params, ServletContext servletContext) throws Exception {
+		
+		/*Obter conexão com banco de dados*/
+		Connection connection = jdbcTemplate.getDataSource().getConnection();
+		
+		/*Carregar o caminho do arquivo Jasper nome por parametro e extenção do arquivo que é o jasper*/
+		String caminhoJasper = servletContext.getRealPath("relatorio")
+				+File.separator + nomeRelatorio + ".jasper";
+		
+		/*Gerar o Relatorio com os dados e conexão*/
+		JasperPrint print = JasperFillManager.fillReport(caminhoJasper,
+				 params, connection);
+		
+		/*Exporta para byte Array o Pdf para fazer o dowload*/
+		byte[] retorno = JasperExportManager.exportReportToPdf(print);
+		
+		connection.close(); //fecha a conexão para evitar algum problema com a conexão
+		
+		return retorno; //ai pega e retorna certinho o nosso pdf e fecha  a conexão q foi usada pelo relatorio
+	}
+	
+	
+
+}
